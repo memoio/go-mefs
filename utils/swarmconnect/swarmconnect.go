56d34256a2291ce586afb6307254a604a96c0509
yydfjt <yydfjt@hotmail.com>
2019-08-26 17:01:59 +0800

    fix swarm connect problem

--- utils/swarmconnect/swarmconnect.go
+++ utils/swarmconnect/swarmconnect.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"strings"
 
+	iaddr "github.com/ipfs/go-ipfs-addr"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	swarm "github.com/libp2p/go-libp2p-swarm"
 	"github.com/memoio/go-mefs/config"
 	"github.com/memoio/go-mefs/core"
 	dht "github.com/memoio/go-mefs/source/go-libp2p-kad-dht"
 	"github.com/memoio/go-mefs/utils/metainfo"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 //连接试三次
@@ -28,7 +30,7 @@ func ConnectTo(ctx context.Context, node *core.MefsNode, to string) bool {
 	connectTryCount := 3
 	for i := 0; i <= connectTryCount; i++ {
 		if retry { // retry three times
-			return getAddrAndConnect(node, to)
+			return getAddrAndConnect(ctx, node, to)
 		}
 
 		pi, err := node.Routing.FindPeer(ctx, id)
@@ -50,18 +52,11 @@ func ConnectTo(ctx context.Context, node *core.MefsNode, to string) bool {
 	return false
 }
 
-func getAddrAndConnect(node *core.MefsNode, to string) bool {
+func getAddrAndConnect(ctx context.Context, node *core.MefsNode, to string) bool {
 	km, err := metainfo.NewKeyMeta(to, metainfo.GetPeerAddr)
 	if err != nil {
 		return false
 	}
-
-	id, err := peer.IDB58Decode(to)
-	if err != nil {
-		return false
-	}
-
-	pi := peer.AddrInfo{}
 
 	for _, defaultBootstrapAddress := range config.DefaultBootstrapAddresses {
 		addr := strings.Split(defaultBootstrapAddress, "/")
@@ -70,8 +65,44 @@ func getAddrAndConnect(node *core.MefsNode, to string) bool {
 		if err != nil {
 			continue
 		}
-		pi.ID = id
-		fmt.Println("get: ", res)
+		paddr := res + "/ipfs/" + to
+
+		fmt.Println("try to connect: ", paddr)
+
+		pai, err := peersWithAddresses(paddr)
+		if err != nil {
+			return false
+		}
+
+		if swrm, ok := node.PeerHost.Network().(*swarm.Swarm); ok {
+			swrm.Backoff().Clear(pai.ID)
+		}
+
+		err = node.PeerHost.Connect(ctx, pai)
+		if err == nil {
+			return true
+		}
+
 	}
 	return false
+}
+
+// peersWithAddresses is a function that takes in a slice of string peer addresses
+// (multiaddr + peerid) and returns a slice of properly constructed peers
+func peersWithAddresses(addrs string) (peer.AddrInfo, error) {
+	pai := peer.AddrInfo{}
+
+	iaddrs, err := iaddr.ParseString(addrs)
+	if err != nil {
+		return pai, err
+	}
+
+	pai.ID = iaddrs.ID()
+	tpt := iaddrs.Transport()
+
+	if tpt != nil {
+		pai.Addrs = []ma.Multiaddr{tpt}
+	}
+
+	return pai, nil
 }
