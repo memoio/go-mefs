334b32870918ec247a0712bfa2620e1331971b4a
yydfjt <yydfjt@hotmail.com>
2019-09-09 16:45:15 +0800

    fix reapir progress in keeper

--- role/keeper/repair.go
+++ role/keeper/repair.go
@@ -14,6 +14,7 @@ import (
 	"github.com/memoio/go-mefs/utils"
 	"github.com/memoio/go-mefs/utils/address"
 	"github.com/memoio/go-mefs/utils/metainfo"
+	sc "github.com/memoio/go-mefs/utils/swarmconnect"
 )
 
 var repch chan string
@@ -33,7 +34,7 @@ func checkrepairlist(ctx context.Context) {
 				uid := cid[:utils.IDLength]
 				if localKeeperIsMaster(uid) {
 					log.Println("repairing cid: ", cid)
-					RepairBlock(uid, cid)
+					RepairBlock(ctx, uid, cid)
 				}
 			case <-ctx.Done():
 				return
@@ -44,9 +45,10 @@ func checkrepairlist(ctx context.Context) {
 
 //RepairBlock works in 3 steps 1.search a new provider,we do it in func SearchNewProvider
 //2.put chunk to this provider 3.change metainfo and sync
-func RepairBlock(userID string, blockID string) {
+func RepairBlock(ctx context.Context, userID string, blockID string) {
 	var cpids, ugid []string
 	var offset int
+	var response string
 	blkinfo, err := metainfo.GetBlockMeta(blockID)
 	if err != nil {
 		log.Println("Get Block Meta error :", blockID, err)
@@ -64,25 +66,35 @@ func RepairBlock(userID string, blockID string) {
 					log.Println("GetBlockMeta error :", err)
 					return false
 				}
-				if blkinfo.GetGid() == blockMeta.GetGid() && blkinfo.GetSid() == blockMeta.GetSid() && blkinfo.GetBid() != blockMeta.GetBid() {
-					cpids = append(cpids, cid+metainfo.REPAIR_DELIMETER+pu.pid)
-					ugid = append(ugid, pu.pid)
-				} else if strings.Compare(cid, blockID) == 0 {
+
+				if strings.Compare(cid, blockID) == 0 {
 					offset = value.(*cidInfo).offset
 					cpids = append(cpids, cid+metainfo.REPAIR_DELIMETER+pu.pid+metainfo.REPAIR_DELIMETER+strconv.Itoa(offset))
 					ugid = append(ugid, pu.pid)
+					response = pu.pid
 				}
+
+				if blkinfo.GetGid() == blockMeta.GetGid() && blkinfo.GetSid() == blockMeta.GetSid() && blkinfo.GetBid() != blockMeta.GetBid() {
+					cpids = append(cpids, cid+metainfo.REPAIR_DELIMETER+pu.pid)
+					ugid = append(ugid, pu.pid)
+				}
+
 				return true
 			})
 		}
 		return true
 	})
-	response := SearchNewProvider(ugid)
-	if response == "" {
-		log.Println("Repair failed, no provider")
-		return
+
+	if len(response) > 0 {
+		if !sc.ConnectTo(ctx, localNode, response) {
+			response = SearchNewProvider(ctx, userID, ugid)
+			if response == "" {
+				log.Println("Repair failed, no extra provider")
+				return
+			}
+		}
 	}
-	log.Println("Repair provider: ", response)
+
 	var rpids string
 	for _, cpid := range cpids {
 		rpids += cpid
@@ -94,7 +106,7 @@ func RepairBlock(userID string, blockID string) {
 		return
 	}
 	metaValue := rpids
-	log.Println("cpids: ", cpids, "\nrpids: ", rpids)
+	log.Println("cpids: ", cpids, "\nrpids: ", rpids, " \nrepairs on: ", response)
 	_, err = sendMetaRequest(km, metaValue, response)
 	if err != nil {
 		log.Println("err: ", err)
@@ -108,63 +120,34 @@ func handleRepairResponse(km *metainfo.KeyMeta, metaValue, provider string) {
 		log.Println("handleRepairResponse err: ", metainfo.ErrIllegalValue, metaValue)
 		return
 	}
-	pid := splitedValue[2]
+	// old provider
+	oldPid := splitedValue[2]
 	offset, err := strconv.Atoi(splitedValue[3])
 	if err != nil {
 		log.Println("strconv.Atoi offset error: ", err)
 		return
 	}
 	uid := blockID[:utils.IDLength]
-	pu := PU{
-		pid: pid,
+	oldpu := PU{
+		pid: oldPid,
 		uid: uid,
 	}
 	if strings.Compare(splitedValue[0], RepairFailed) == 0 {
 		log.Println("repair failed, cid is: ", blockID)
-		thischalinfo, ok := getChalinfo(pu)
-		if ok {
-			if thiscidinfo, ok := thischalinfo.Cid.Load(blockID); ok {
-				thiscidinfo.(*cidInfo).res = false
-				thiscidinfo.(*cidInfo).repair = 0
-			}
-		} else {
-			log.Println("!ok blockID :", blockID, "\npid :", pid, "\nuid :", uid)
-			isTestUser := false
-			addr, err := address.GetAddressFromID(pu.uid)
-			if err == nil {
-				_, _, err = contracts.GetUKFromResolver(addr)
-				if err != nil {
-					isTestUser = true
-				}
-			}
-
-			newcidinfo := &cidInfo{
-				repair: 0,
-				offset: offset,
-				res:    false,
-			}
-			var newCid, newTime sync.Map
-			newCid.Store(blockID, newcidinfo)
-			newchalinfo := &chalinfo{
-				Time:     newTime,
-				Cid:      newCid,
-				testuser: isTestUser,
-			}
-			LedgerInfo.Store(pu, newchalinfo)
-		}
 	} else {
-		pu1 := PU{
+		log.Println("repair success, cid is: ", blockID)
+		newpu := PU{
 			pid: provider,
 			uid: uid,
 		}
-		log.Println("repair success, cid is: ", blockID)
+
 		newcidinfo := &cidInfo{
 			repair:    0,
 			availtime: utils.GetUnixNow(),
 			offset:    offset,
 		}
 
-		if thischalinfo, ok := getChalinfo(pu1); ok {
+		if thischalinfo, ok := getChalinfo(newpu); ok {
 			if thischalinfo.inChallenge == 1 {
 				thischalinfo.tmpCid.Store(blockID, newcidinfo)
 			} else if thischalinfo.inChallenge == 0 {
@@ -172,7 +155,7 @@ func handleRepairResponse(km *metainfo.KeyMeta, metaValue, provider string) {
 			}
 		} else {
 			isTestUser := false
-			addr, err := address.GetAddressFromID(pu1.uid)
+			addr, err := address.GetAddressFromID(newpu.uid)
 			if err == nil {
 				_, _, err = contracts.GetUKFromResolver(addr)
 				if err != nil {
@@ -187,10 +170,10 @@ func handleRepairResponse(km *metainfo.KeyMeta, metaValue, provider string) {
 				Cid:      newCid,
 				testuser: isTestUser,
 			}
-			LedgerInfo.Store(pu1, newchalinfo)
+			LedgerInfo.Store(newpu, newchalinfo)
 		}
 
-		oldchalinfo, isExist := getChalinfo(pu)
+		oldchalinfo, isExist := getChalinfo(oldpu)
 		if isExist {
 			oldchalinfo.Cid.Delete(blockID)
 		}
@@ -212,37 +195,40 @@ func handleRepairResponse(km *metainfo.KeyMeta, metaValue, provider string) {
 				NewPids += Pid
 			}
 		}
+
+		// new provider, now there is no new provider
 		if flag == len(thisGroupsInfo.Providers) {
 			thisGroupsInfo.Providers = append(thisGroupsInfo.Providers, provider)
-		}
-		NewPids += provider
+			NewPids += provider
 
-		kmPid, err := metainfo.NewKeyMeta(uid, metainfo.Sync, metainfo.SyncTypePid)
-		if err != nil {
-			log.Println("construct SyncPidsK error :", err)
-			return
+			kmPid, err := metainfo.NewKeyMeta(uid, metainfo.Sync, metainfo.SyncTypePid)
+			if err != nil {
+				log.Println("construct SyncPidsK error :", err)
+				return
+			}
+			metaSyncTo(kmPid, NewPids)
+			kmPid.SetKeyType(metainfo.Local) //将数据格式转换为local 保存在本地
+			err = localNode.Routing.(*dht.IpfsDHT).CmdPutTo(kmPid.ToString(), NewPids, "local")
+			if err != nil {
+				log.Println("construct SyncPidsK error :", err)
+				return
+			}
+			//更新block的meta信息
+			kmBlock, err := metainfo.NewKeyMeta(blockID, metainfo.Sync, metainfo.SyncTypeBlock)
+			if err != nil {
+				log.Println("construct Syncblock KV error :", err)
+				return
+			}
+			metaValue := provider + metainfo.DELIMITER + strconv.Itoa(offset)
+			metaSyncTo(kmBlock, metaValue)
+			kmBlock.SetKeyType(metainfo.Local) //将数据格式转换为local 保存在本地
+			err = localNode.Routing.(*dht.IpfsDHT).CmdPutTo(kmBlock.ToString(), metaValue, "local")
+			if err != nil {
+				log.Println("construct SyncPidsK error :", err)
+				return
+			}
 		}
-		metaSyncTo(kmPid, NewPids)
-		kmPid.SetKeyType(metainfo.Local) //将数据格式转换为local 保存在本地
-		err = localNode.Routing.(*dht.IpfsDHT).CmdPutTo(kmPid.ToString(), NewPids, "local")
-		if err != nil {
-			log.Println("construct SyncPidsK error :", err)
-			return
-		}
-		//更新block的meta信息
-		kmBlock, err := metainfo.NewKeyMeta(blockID, metainfo.Sync, metainfo.SyncTypeBlock)
-		if err != nil {
-			log.Println("construct Syncblock KV error :", err)
-			return
-		}
-		metaValue := provider + metainfo.DELIMITER + strconv.Itoa(offset)
-		metaSyncTo(kmBlock, metaValue)
-		kmBlock.SetKeyType(metainfo.Local) //将数据格式转换为local 保存在本地
-		err = localNode.Routing.(*dht.IpfsDHT).CmdPutTo(kmBlock.ToString(), metaValue, "local")
-		if err != nil {
-			log.Println("construct SyncPidsK error :", err)
-			return
-		}
+
 	}
 	return
 
@@ -251,23 +237,41 @@ func handleRepairResponse(km *metainfo.KeyMeta, metaValue, provider string) {
 //SearchNewProvider find a NEW provider for user
 //TODO:how to improve the search algorithm
 //TODO:is a Timer needed？
-func SearchNewProvider(ugid []string) string {
-	var response string                                  //return the provider id we need
+func SearchNewProvider(ctx context.Context, uid string, ugid []string) string {
+	var response string
+	gp, ok := getGroupsInfo(uid)
+	if !ok {
+		log.Println("SearchNewProvider getGroupsInfo() error")
+		return response
+	}
+
+	lenp := len(gp.Providers)
+
+	if lenp == 0 || lenp <= len(ugid) {
+		return response
+	}
+
+	retry := 0
+	//return the provider id we need
 	r := rand.New(rand.NewSource(time.Now().UnixNano())) //r is a time seed.we use it to create the random number
 	for {
-		if len(localPeerInfo.Providers) == 0 {
-			return ""
-		}
-		response = localPeerInfo.Providers[r.Intn(len(localPeerInfo.Providers))] //first we find a random provider
+		retry++
+		response = gp.Providers[r.Intn(lenp)] //first we find a random provider
 		var j, flag int
 		flag = 0
-		for j = 0; j < len(ugid); j++ { //this provider may belong to this user already
+		for j = 0; j < len(ugid); j++ { //this provider may belong to this stripe already
 			if response != ugid[j] {
 				flag++
 			}
 		}
 		if flag == len(ugid) {
-			break
+			if sc.ConnectTo(ctx, localNode, response) {
+				break
+			}
+		}
+
+		if retry > 5*len(ugid) {
+			return ""
 		}
 	}
 	return response
