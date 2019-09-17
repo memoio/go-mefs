593274191c4a7f27a7ec1f4787db0c89c39fd921
yydfjt <yydfjt@hotmail.com>
2019-09-17 09:51:17 +0800

    fix contartc test err

--- test/unit/upkeepingTest/upkeepingTest.go
+++ test/unit/upkeepingTest/upkeepingTest.go
@@ -45,7 +45,7 @@ func main() {
 	for {
 		time.Sleep(30 * time.Second)
 		balance := queryBalance(userAddr)
-		if balance.Cmp(big.NewInt(10000000000)) > 0 {
+		if balance.Cmp(big.NewInt(10000000000)) >= 0 {
 			break
 		}
 
