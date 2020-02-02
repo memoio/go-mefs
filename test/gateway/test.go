abc0ef511b24f39f3bded5738bdef2111ad32723
yydfjt.Pro13 <yydfjt@hotmail.com>
2020-02-02 16:59:25 +0800

    fix minio err

--- test/gateway/test.go
+++ test/gateway/test.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/minio/minio-go/v6"
 )
@@ -11,25 +12,48 @@ func main() {
 	ssl := false
 
 	// Initialize minio client object.
-	minioClient, err := minio.New("127.0.0.1:5080", "user", "12345678", ssl)
+	minioClient, err := minio.New("127.0.0.1:5080", "0x7aD8AA67aFEE05Fd539B938db69957050dDDA1c3", "123456789", ssl)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		return
 	}
 
-	fmt.Println("Successfully link client.")
+	log.Println("Successfully link client.")
 
 	found, err := minioClient.BucketExists("mybucket")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	fmt.Println(found)
-	err = minioClient.MakeBucket("mybucket", "us-east-1")
+
+	if !found {
+		err = minioClient.MakeBucket("mybucket", "us-east-1")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("Successfully created mybucket.")
+	}
+
+	file, err := os.Open("./test.go")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	fmt.Println("Successfully created mybucket.")
+	defer file.Close()
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	n, err := minioClient.PutObject("mybucket", "/test/myobject", file, fileStat.Size(), minio.PutObjectOptions{ContentType: "application/octet-stream"})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Successfully uploaded bytes: ", n)
+
 	return
 }
