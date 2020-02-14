6cf43b101fcdd3416e81d6c6110da1259f87317d
yydfjt.Pro13 <yydfjt@hotmail.com>
2020-02-14 11:36:46 +0800

    fix test

---
+++ bin/lfsTest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sh ./checkbin.sh
+
+echo -e "mefs lfs test"
+
+echo -e "\nstep 1,mefs init\n"
+mefs init --netKey=$2
+
+echo -e "\nstep 2,run mefs daemon\n"
+mefs daemon --netKey=$2 >> ~/daemon.stdout 2>&1 &
+echo -e "\ndaemon is ready wait 1min to connect"
+time sleep 60
+
+echo -e "\nstep 3,run lfs test\n"
+GO111MODULE=off go run $GOPATH/src/github.com/memoio/go-mefs/test/lfs/test.go -eth=$1
